27464556faa73d0471aa221f7685559e91380bb5
Said NAJIM <saidnajim.ma@gmail.com>
2025-01-28 10:55:39 +0100

    Ajout couronne

--- Assets/Scripts/a_Champion.cs
+++ Assets/Scripts/a_Champion.cs
@@ -7,12 +7,16 @@ public class a_Champion : MonoBehaviour
     public a_Health Health { get; private set; }
     public a_Attack Attack { get; private set; }
     public a_Movement Movement { get; private set; }
-
     public HealthBar barPrefab;
+    public GameObject crownPrefab; 
     protected HealthBar healthBar;
+    protected GameObject crownInstance;
     protected bool dead = false;
-
+    protected bool isKing = false;
+    public bool IsKing => isKing;
     public List<a_Item> Items { get; private set; } = new();
+
+    [SerializeField] private Vector3 crownOffset = new Vector3(0, 0.5f, 0); 
 
     public void Initialize(a_Entity entity, a_Health health, a_Attack attack, a_Movement movement)
     {
@@ -20,7 +24,6 @@ public class a_Champion : MonoBehaviour
         Health = health;
         Attack = attack;
         Movement = movement;
-
         SetupHealthBar();
     }
 
@@ -28,6 +31,33 @@ public class a_Champion : MonoBehaviour
     {
         healthBar = Instantiate(barPrefab, this.transform);
         healthBar.Setup(this.transform, Health.maxHealth);
+    }
+
+    public void SetKingStatus(bool kingStatus)
+    {
+        if (isKing == kingStatus) return; 
+
+        isKing = kingStatus;
+
+        if (isKing)
+        {
+            if (crownInstance == null && crownPrefab != null)
+            {
+                crownInstance = Instantiate(crownPrefab, transform);
+                crownInstance.transform.localPosition = crownOffset;
+            }
+            if (crownInstance != null)
+            {
+                crownInstance.SetActive(true);
+            }
+        }
+        else
+        {
+            if (crownInstance != null)
+            {
+                crownInstance.SetActive(false);
+            }
+        }
     }
 
     public void TakeDamage(float amount)
@@ -38,6 +68,14 @@ public class a_Champion : MonoBehaviour
         {
             dead = true;
             GameManager.Instance.UnitDead(this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (crownInstance != null)
+        {
+            Destroy(crownInstance);
         }
     }
 }
